255131300d4677879a92d61d886f76441f58987a
driverInside <hiram.eps@gmail.com>
2020-10-12 07:37:02 -0500

    first commit

---
+++ functions.js
@@ -0,0 +1,11 @@
+/**
+ * functions.js
+ *
+ */
+function add (a, b) {
+  const c = a + b
+  return c
+}
+
+console.log(add(3, 2)) // 5
+console.log(add(2, 10)) // 12
---
+++ functions.py
@@ -0,0 +1,10 @@
+'''
+functions.py
+'''
+def add (a, b):
+    c = a + b
+    return c
+
+if __name__ == "__main__":
+    print(add(2, 3)) # 5
+    print(add(2, 10)) #12
